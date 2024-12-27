cf36a86e8ed355dac3bdd979251b3d78056abf6d
Haneena <pphaneena02@gmail.com>
2024-12-27 14:39:39 +0530

    backend folder added

--- Front-end/shoezone/src/pages/Home.jsx
+++ Front-end/shoezone/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Mainshooo from '/assets/mainshoe.jpg'
+import Shop from "./Shop";
+import Footer from "../Components/Footer";
+function Home() {
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+        <div className="w-full flex justify-center items-center">
+                <img 
+                    src={Mainshooo}
+                    alt="Main Shoe"
+                    className="w-full h-96 object-cover" 
+                />
+        </div>
+        <h1 className="font-extrabold text-3xl text-center mt-2 mb-5 bg-black text-white p-4 rounded-lg shadow-lg">LATEST COLLECTIONS</h1>
+
+        <Shop />
+        <Footer />
+            
+        </div>
+    );
 }
 
-export default Home
+export default Home;
+
